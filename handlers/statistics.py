dd3c20b7be426dc40ac185dc7ff539d375a2f7db
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-11 10:44:00 -0300

    Fix database conflict

--- handlers/statistics.py
+++ handlers/statistics.py
@@ -6,6 +6,6 @@ import logging
 
 
 @pyding.on("status_ping", priority=100)
-def statistics_log(event, status: tweepy.Status, client, api):
-    database.add_tweet(status.id, status.text, status.media, user_id=status.user.id)
+def statistics_log(event, status, client, api):
+    #database.add_tweet(status.id, status.text, status.media, user_id=status.user.id)
     database.add_mention_entry(status.id, status.text, f"https://twitter.com/{status.user.screen_name}/status/{status.id}")
